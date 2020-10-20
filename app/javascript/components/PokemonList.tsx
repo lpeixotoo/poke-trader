90cf1d96b62b627b60091c725f41c74283736671
Lucas Peixoto <37353676+lpeixotoo@users.noreply.github.com>
2020-10-20 01:37:24 -0300

    Add list interactions (#32)
    - Add prettier and icons packages
    - Add state handlers for adding/removing pokemons

--- app/javascript/components/PokemonList.tsx
+++ app/javascript/components/PokemonList.tsx
@@ -1,22 +1,49 @@
 import * as React from "react"
 import { Pokemon } from "./types"
 import { useState, useEffect } from "react"
-import PokemonListCard from "./PokemonListCard"
+import { IoIosAddCircleOutline } from "react-icons/io"
 
-const PokemonList: React.FC = () => {
+export type Props = {
+  addAshHandlerBuilder: (
+    pokemon: Pokemon,
+  ) => (event: React.MouseEvent<HTMLAnchorElement>) => void
+  addBrockHandlerBuilder: (
+    pokemon: Pokemon,
+  ) => (event: React.MouseEvent<HTMLAnchorElement>) => void
+}
+
+const PokemonList: React.FC<Props> = ({
+  addAshHandlerBuilder,
+  addBrockHandlerBuilder,
+}: Props) => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([])
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/v1/pokemons')
-      .then(response => response.json())
-      .then(response => setPokemons(response))
-  }, []);
+    fetch("http://localhost:3000/api/v1/pokemons")
+      .then((response) => response.json())
+      .then((response) => setPokemons(response))
+  }, [])
 
   return (
     <ul className="list-unstyled">
-      {
-        pokemons.map((pokemon, index) => <PokemonListCard pokemon={pokemon} />)
-      }
+      {pokemons.map((pokemon, index) => {
+        return (
+          <li key={index} className="media">
+            <img className="align-self-center mr-3" src={pokemon.sprite} />
+            <div className="media-body">
+              <h5 className="mt-0 mb-1">{pokemon.name}</h5>
+              <p>Base Experience: {pokemon.base_experience}</p>
+              <p>HP: {pokemon.hp_stats}</p>
+              <a onClick={addAshHandlerBuilder(pokemon)}>
+                <IoIosAddCircleOutline />
+              </a>
+              <a onClick={addBrockHandlerBuilder(pokemon)}>
+                <IoIosAddCircleOutline />
+              </a>
+            </div>
+          </li>
+        )
+      })}
     </ul>
   )
 }
--- app/javascript/components/Trade.tsx
+++ app/javascript/components/Trade.tsx
@@ -2,19 +2,94 @@ import * as React from "react"
 import { Pokemon } from "./types"
 import { useState, useEffect } from "react"
 import PokemonList from "./PokemonList"
+import TradeSideList from "./TradeSideList"
 
 const Trade: React.FC = () => {
+  const [sideAshPokemons, setSideAshPokemons] = useState<Pokemon[]>([])
+  const [sideBrockPokemons, setSideBrockPokemons] = useState<Pokemon[]>([])
+
+  // add pokemons to side handlers
+  const addSideAshHandlerBuilder = (newPokemon: Pokemon) => {
+    return () =>
+      setSideAshPokemons((prevPokemons) =>
+        //TODO: Set maximum as config variable
+        prevPokemons.length === 6
+          ? [...prevPokemons]
+          : [...prevPokemons, newPokemon],
+      )
+  }
+  const addSideBrockHandlerBuilder = (newPokemon: Pokemon) => {
+    return () =>
+      setSideBrockPokemons((prevPokemons) =>
+        prevPokemons.length === 6
+          ? [...prevPokemons]
+          : [...prevPokemons, newPokemon],
+      )
+  }
+
+  //remove pokemons from side handlers
+  const removeSideAshHandlerBuilder = (targetPokemonIndex: number) => {
+    return () =>
+      setSideAshPokemons((prevPokemons) =>
+        prevPokemons.filter((_, index) => index !== targetPokemonIndex),
+      )
+  }
+  const removeSideBrockHandlerBuilder = (targetPokemonIndex: number) => {
+    return () =>
+      setSideBrockPokemons((prevPokemons) =>
+        prevPokemons.filter((_, index) => index !== targetPokemonIndex),
+      )
+  }
+
+  // base experience calculations variables
+  const totalBaseExperienceAsh = sideAshPokemons.reduce(
+    (acc, pokemon) => acc + pokemon.base_experience,
+    0,
+  )
+  const totalBaseExperienceBrock = sideBrockPokemons.reduce(
+    (acc, pokemon) => acc + pokemon.base_experience,
+    0,
+  )
+  const baseExperienceDifference = Math.abs(
+    totalBaseExperienceAsh - totalBaseExperienceBrock,
+  )
+
+  //TODO: Set acceptable difference as config variable
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-2">
-          <PokemonList />
+          <PokemonList
+            addAshHandlerBuilder={addSideAshHandlerBuilder}
+            addBrockHandlerBuilder={addSideBrockHandlerBuilder}
+          />
         </div>
-        <div className="col-5">
-          2 of 3
-        </div>
-        <div className="col-5">
-          3 of 3
+        <div className="col-10">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col">
+                {baseExperienceDifference > 40 ? "Not Fair" : "Fair"}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <h1>Side Ash</h1>
+                <TradeSideList
+                  pokemons={sideAshPokemons}
+                  totalBaseExperience={totalBaseExperienceAsh}
+                  removePokemonHandlerBuilder={removeSideAshHandlerBuilder}
+                />
+              </div>
+              <div className="col">
+                <h1>Side Brock</h1>
+                <TradeSideList
+                  pokemons={sideBrockPokemons}
+                  totalBaseExperience={totalBaseExperienceBrock}
+                  removePokemonHandlerBuilder={removeSideBrockHandlerBuilder}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
